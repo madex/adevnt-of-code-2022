244ce78c3ac779a6aae5157d57719bfccb9bbc00
Martin Beokhoff <martin.boekhoff@dorma.com>
2022-12-17 12:15:42 +0100

    day13

--- python/day13/solution.py
+++ python/day13/solution.py
@@ -19,24 +19,23 @@ def compare(a, b):
             return -1
     return 0
 
-f = open('test.txt')
+f = open('input.txt')
 m = [[eval(c) for c in x.split('\n')] for x in f.read().strip().split('\n\n')]
 f.close()
 right_order = []
-for i, pair in enumerate(m):
+for i, pair in enumerate(m, start=1):
     if compare(pair[0], pair[1]) == 1:
-        right_order.append(i + 1)
+        right_order.append(i)
 
 print("Part 1:", sum(right_order))
 
 packets = [p[i] for p in m for i in range(2)]
 packets.append([[2]])
 packets.append([[6]])
-t = [str(p).replace('[',"").replace(']',"").replace(',',"").replace(' ',"") for p in packets]
-p2 = 1
-for i, s in enumerate(sorted(t)):
-    if s == '2' or s == '6':
-        print(i+1, s)
-        p2 *= i + 1
-#print(sorted(t))
-print("Part 2:", p2)
+from functools import cmp_to_key
+packets = sorted(packets, key=cmp_to_key(lambda p1, p2: compare(p2, p1)))
+part2 = 1
+for i,p in enumerate(packets, start=1):
+    if p==[[2]] or p==[[6]]:
+        part2 *= i
+print("Part 2:", part2)
