e8b637c8efe190f42dc54f8d476085517ecd56ef
Martin Beokhoff <martin.boekhoff@dorma.com>
2022-12-17 23:59:04 +0100

    day15 python fix preformance issuses. Part2 is still slow.

--- python/day15/solution.py
+++ python/day15/solution.py
@@ -1,3 +1,28 @@
+def check(y, part1):
+    blocked = []
+    for e in m:
+        d = abs(e[0] - e[2]) + abs(e[1] - e[3])
+        a = abs(e[1] - y)
+        if a <= d:
+            d0 = e[0] - d + a
+            d1 = e[0] + d - a
+            blocked.append([d0, d1]) 
+    blocked.sort()
+    b = blocked.pop(0)
+    if b[0] > 0:
+        return 0
+    for x in blocked:
+        if b[0] <= x[0] <= b[1]:
+            if x[1] > b[1]:
+                b[1] = x[1]
+        else:
+            return x[0] - 1
+    if part1:
+        return b[1] - b[0] + 1 
+    elif  b[1] < cMax:
+        return cMax
+    return -1
+
 import re
 f = open('input.txt')
 m = []
@@ -6,31 +31,18 @@ for line in f.read().strip().split('\n'):
     if match:
         m.append([int(match.group(i)) for i in range(1, 5)])
 f.close()
-y =  2000000
-blocked = set()
-for e in m:
-    d = abs(e[0] - e[2]) + abs(e[1] - e[3])
-    e.append(d)
-    a = abs(e[1] - y)
-    for x in range(e[0] - d + a , e[0] + d - a + 1):
-        if x not in blocked and abs(e[0] - x) + a <= d:
-            blocked.add(x)
+y = 2000000 # 10
+blocked = check(y, True)
 # remove x from beacons
+rem = set()
 for e in m:
     if e[3] == y:
-        blocked.discard(e[2])
+        rem.add(e[2])
+print("Part 1:", blocked - len(rem))
+cMax = 4000000 # 20
 
-print("Part 1:", len(blocked))
-cMax = 400000
-
-def check(x, y):
-    for e in m:
-        if abs(e[0] - x) + abs(e[1] - y) <= e[4]:
-            return True
-    return False
-
-for x in range(cMax + 1):
-    for y in range(cMax + 1):
-        if not check(x, y):
-            print("Part 2:", 4000000*x + y)
-            break
+for y in range(cMax + 1):
+    x = check(y, False)
+    if x != -1:
+        print("Part 2:", 4000000*x + y)
+        break
